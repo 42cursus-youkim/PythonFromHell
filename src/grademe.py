7b069731a823f5f760234786457b27afccca3437
youkim <42.4.youkim@gmail.com>
2021-11-02 17:32:01 +0900

    feat: :sparkles: for loop interface for  pytest in grader

---
+++ src/grademe.py
@@ -0,0 +1,7 @@
+from pymoulinette import Grader
+
+if __name__ == "__main__":
+    grader = Grader("src/pymoulinette/day00")
+    print(grader)
+    grader.grade()
+
--- src/pymoulinette/grader.py
+++ src/pymoulinette/grader.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from sys import argv
 from typing import List
 
-from submits import Answers, Submits
-from utils import Day, Ex, err
+from .submits import Answers, Submits
+from .utils import Day, Ex, err
 
 
 class Grader:
@@ -14,7 +14,13 @@ class Grader:
     def __str__(self):
         return f"Grader loaded with submits and answers from <{self.name}>"
 
+    def grade(self):
+        for ex_a, ex_s in zip(self.answer, self.submit):
+            self.grade_ex(ex_a, ex_s)
 
-if __name__ == "__main__":
-    grader = Grader("src/pymoulinette/day00")
-    print(grader)
+    def grade_exlist(self, answers: List[Ex], submits: List[Ex]):
+        for py_s, py_a in zip(answers, submits):
+            self.grade_ex(py_a, py_s)
+
+    def grade_ex(self, answer: Ex, submit: Ex):
+        raise NotImplementedError("TODO")

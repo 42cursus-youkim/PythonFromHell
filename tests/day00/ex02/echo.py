9d9a5be0e8561ba76c13d97f9e134849a75b297b
Junghun Kim <junghkim@c3r4s1.42seoul.kr>
2021-11-03 19:40:31 +0900

    Revert "chore: :truck: MOVE EVERYTHING INTO PACKAGE"
    This reverts commit d489af00a907f59fd905f9e26fabaf11d88b86e9.

---
+++ tests/day00/ex02/echo.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+
+def main():
+    for arg in argv[1:]:
+        print(arg)
---
+++ tests/day00/ex03/my_vars.py
@@ -0,0 +1,5 @@
+def answer(spam: int):
+    print(spam)
+    print(hex(id(spam)))
+    print(type(spam))
+    print(spam + 11)
